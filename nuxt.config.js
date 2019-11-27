9485da280c7e00e8c634702161484e895a913deb
bohdan <liba@moqod.com>
2019-11-27 23:25:55 +0100

    Stylelint removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,8 +34,6 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
   ],
   /*
    ** Nuxt.js modules
